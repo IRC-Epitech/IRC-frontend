ed4e27c906dafde919a44686e59424250b42e449
Antoine <gourgue.antoine@esh64.fr>
2024-01-30 14:01:49 +0100

    feat: make messsagesender, messagereceiver

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,50 +1,13 @@
-    const MessageBox =() => {
+import MessageSender from "./MessageSender.jsx";
+import MessageReceiver from "./MessageReceiver.jsx";
+
+const MessageBox =() => {
         return (
 
             <div className="w-full px-5 flex flex-col justify-between min-h-screen">
                 <div className="flex flex-col mt-5">
-
-                    <div className="flex justify-start mb-4">
-                        <img
-                            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                            className="object-cover h-8 w-8 rounded-full"
-                            alt=""
-                        />
-                        <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Quaerat at praesentium, aut ullam delectus odio error sit rem.
-                            Architecto nulla doloribus laborum illo rem enim dolor odio
-                            saepe, consequatur quas?
-                        </div>
-                    </div>
-                    <div className="flex justify-end mb-4">
-                        <div>
-                            <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                                Magnam, repudiandae.
-                            </div>
-
-                            <div className="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                Debitis, reiciendis!
-                            </div>
-                        </div>
-                        <img
-                            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                            className="object-cover h-8 w-8 rounded-full"
-                            alt=""
-                        />
-                    </div>
-                    <div className="flex justify-start mb-4">
-                        <img
-                            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                            className="object-cover h-8 w-8 rounded-full"
-                            alt=""
-                        />
-                        <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-                            happy holiday guys!
-                        </div>
-                    </div>
+                    <MessageSender/>
+                    <MessageReceiver/>
                 </div>
                 <div className="py-5">
                     <input
