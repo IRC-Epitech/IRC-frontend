ab790db6ed78ae9ad2b3c146dc9aafc7fd20903f
Fabien <fabien.le-fichoux@epitech.eu>
2024-02-05 10:33:03 +0100

    Add button file upload

---
+++ src/components/buttonFileUpload.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function FileUploadButton() {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        setSelectedFile(e.target.files[0]);
+    };
+
+    const handleUpload = async () => {
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+
+        try {
+            const response = await axios.post('/fileuploads', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error('Erreur lors de l\'upload du fichier:', error);
+        }
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+        </div>
+    );
+}
+
+export default FileUploadButton;
